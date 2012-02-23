e8e4f246b14418323656727bebd607a0c6402d46
Rafael Lima <contato@rafael.adm.br>
2012-02-23 00:23:50 -0200

    refactory of all methods to accept more than one session_id simultaneously

--- lib/interfax.rb
+++ lib/interfax.rb
@@ -16,14 +16,14 @@ class Interfax
   end
 
   def start_file_upload
-    @session_id ||= request :start_file_upload do |response|
-      return @session_id = response[:session_id]
+    @last_session_id = request :start_file_upload do |response|
+      response[:session_id]
     end
   end
 
   def upload_file_chunk(file_path, options = {})
     chunk_size = options.delete(:chunk_size) || 200000
-    @session_id ||= start_file_upload
+    session_id = options.delete(:session_id) || @last_session_id || start_file_upload
     @file_path = file_path
 
     each_chunk(chunk_size) do |chunk, is_last|
@@ -38,23 +38,25 @@ class Interfax
     return @bytes_uploaded == File.size(@file_path)
   end
 
-  def cancel_file_upload
-    return if @session_id.nil?
-    request :cancel_file_upload, :session_id => @session_id
+  def cancel_file_upload(options = {})
+    session_id = options.delete(:session_id) || @last_session_id
+    return if session_id.nil?
+    request :cancel_file_upload, :session_id => session_id
   end
 
   def sendfax_ex_2(options = {})
-    if !@bytes_uploaded.zero?
-      options.merge!(
-        :file_types => File.extname(@file_path)[1..4].upcase,
-        :file_sizes => "#{@bytes_uploaded}/sessionID=#{@session_id}"
-      )
-    end
-    options[:retries_to_perform] ||= 1
-    options[:page_size] ||= 'A4'
-    options[:page_orientation] ||= 'Portrait'
-    options[:is_high_resolution] ||= false
-    options[:is_fine_rendering] ||= true
+    default_options = {
+      :retries_to_perform => 1,
+      :page_size => 'A4',
+      :page_orientation => 'Portrait',
+      :is_high_resolution => false,
+      :is_fine_rendering => true,
+    }    
+    default_options.merge!(
+      :file_types => File.extname(@file_path)[1..4].upcase,
+      :file_sizes => "#{@bytes_uploaded}/sessionID=#{@last_session_id}"
+    ) unless @bytes_uploaded.zero?
+    options = default_options.merge(options)
     options[:is_high_resolution] = options[:is_high_resolution] ? 1 : 0 unless options[:is_high_resolution].is_a? Integer
     options[:is_fine_rendering] = options[:is_fine_rendering] ? 1 : 0 unless options[:is_fine_rendering].is_a? Integer
     request :sendfax_ex_2, options
@@ -66,8 +68,8 @@ class Interfax
     return false unless raw_response.success?
     result = raw_response.to_hash["#{method_name}_response".to_sym]["#{method_name}_result".to_sym].to_i
     response = raw_response.to_hash["#{method_name}_response".to_sym]
-    yield response, result if block_given?
-    return result >= 0
+    return result >= 0 unless block_given?
+    yield response, result
   end
 
   # reads the given +file+ in chunks set though +chunk_size+ and yields
--- spec/interfax_spec.rb
+++ spec/interfax_spec.rb
@@ -35,10 +35,10 @@ describe "Interfax" do
   describe "#start_file_upload" do
     context "authorized" do
       let(:interfax) { Interfax.new(:username => 'rafael_lima', :password => ENV['INTERFAX_PASSWORD_FOR_RSPEC']) }
-      it "should assign session_id" do
+      it "should assign last_session_id" do
         VCR.use_cassette('start_file_upload_authorized') do
           interfax.start_file_upload
-          interfax.instance_variable_get(:@session_id).should == session_id
+          interfax.instance_variable_get(:@last_session_id).should == session_id
         end
       end
       it "should return session id" do
@@ -46,8 +46,8 @@ describe "Interfax" do
           interfax.start_file_upload.should == session_id
         end
       end
-      it "should cache session id and not request twice" do
-        interfax.client.should_receive(:request).once.and_return(mock("Response", :success? => true, :to_hash => {:start_file_upload_response => {:start_file_upload_result => "1", :session_id => "2FE69E6C86DB4CB4926A94E63B68039AD83BCBB7F10C407F9641FEAEF5EA38EE"}}))
+      it "should not cache session id" do
+        interfax.client.should_receive(:request).twice.and_return(mock("Response", :success? => true, :to_hash => {:start_file_upload_response => {:start_file_upload_result => "1", :session_id => "2FE69E6C86DB4CB4926A94E63B68039AD83BCBB7F10C407F9641FEAEF5EA38EE"}}))
         VCR.use_cassette('start_file_upload_authorized') do
           2.times { interfax.start_file_upload }
         end
@@ -59,20 +59,29 @@ describe "Interfax" do
           interfax.start_file_upload.should be_nil
         end
       end
-      it "should not assign session_id" do
+      it "should not assign last_session_id" do
         VCR.use_cassette('start_file_upload_unauthorized') do
           interfax.start_file_upload
-          interfax.instance_variable_get(:@session_id).should be_nil
+          interfax.instance_variable_get(:@last_session_id).should be_nil
         end
       end
     end
   end
   describe "#upload_file_chunk" do
     context "start_file_upload was not called" do
-      it "should call start_file_upload" do
-        interfax.should_receive(:start_file_upload)
-        interfax.stub(:request)
-        interfax.upload_file_chunk(File.join('spec','fixtures','mickey01.jpg'))
+      context "session id is not passed" do
+        it "should call start_file_upload" do
+          interfax.should_receive(:start_file_upload)
+          interfax.stub(:request)
+          interfax.upload_file_chunk(File.join('spec','fixtures','mickey01.jpg'))
+        end
+      end
+      context "session id is passed" do
+        it "should not call start_file_upload" do
+          interfax.should_not_receive(:start_file_upload)
+          interfax.stub(:request)
+          interfax.upload_file_chunk(File.join('spec','fixtures','mickey01.jpg'), :session_id => session_id)
+        end
       end
     end
     context "start_file_upload was called before" do
@@ -102,8 +111,17 @@ describe "Interfax" do
   end
   describe "#cancel_file_upload" do
     context "start_file_upload was not successfully called" do
-      it "should return nil" do
-        interfax.cancel_file_upload.should be_nil
+      context "session id is not passed" do
+        it "should return nil" do
+          interfax.cancel_file_upload.should be_nil
+        end
+      end
+      context "session id is passed" do
+        it "should return true" do
+          VCR.use_cassette('cancel_file_upload_with_session') do
+            interfax.cancel_file_upload(:session_id => session_id).should be_true
+          end
+        end
       end
     end
     context "start_file_upload was successfully called" do
@@ -120,7 +138,29 @@ describe "Interfax" do
     end
   end
   describe "#sendfax_ex_2" do
-    context "without options" do
+    context "with file uploaded" do
+      before(:each) do
+        VCR.use_cassette('start_file_upload_authorized') do
+          interfax.start_file_upload
+        end
+        VCR.use_cassette('upload_file_chunk_authorized') do
+          interfax.upload_file_chunk(File.join('spec','fixtures','mickey01.jpg'), :chunk_size => 40960).should be_true
+        end
+      end
+      it "should set default values" do
+        interfax.should_receive(:request).with(:sendfax_ex_2, {
+          :file_types => 'JPG',
+          :file_sizes => "68702/sessionID=#{session_id}",
+          :retries_to_perform => 1,
+          :page_size => 'A4',
+          :page_orientation => 'Portrait',
+          :is_high_resolution => 0,
+          :is_fine_rendering => 1,
+        })
+        interfax.sendfax_ex_2
+      end
+    end
+    context "without file uploaded" do
       it "should set default values" do
         interfax.should_receive(:request).with(:sendfax_ex_2, {
           :retries_to_perform => 1,
@@ -130,28 +170,6 @@ describe "Interfax" do
           :is_fine_rendering => 1,
         })
         interfax.sendfax_ex_2
-      end
-      context "with file uploaded" do
-        before(:each) do
-          VCR.use_cassette('start_file_upload_authorized') do
-            interfax.start_file_upload
-          end
-          VCR.use_cassette('upload_file_chunk_authorized') do
-            interfax.upload_file_chunk(File.join('spec','fixtures','mickey01.jpg'), :chunk_size => 40960).should be_true
-          end
-        end
-        it "should set default values" do
-          interfax.should_receive(:request).with(:sendfax_ex_2, {
-            :file_types => 'JPG',
-            :file_sizes => "68702/sessionID=#{session_id}",
-            :retries_to_perform => 1,
-            :page_size => 'A4',
-            :page_orientation => 'Portrait',
-            :is_high_resolution => 0,
-            :is_fine_rendering => 1,
-          })
-          interfax.sendfax_ex_2
-        end
       end
     end
   end
